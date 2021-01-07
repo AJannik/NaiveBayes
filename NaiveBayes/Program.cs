30a607415cd082eb166c6a3db511f8baf280c521
AJannik <jannik.schmidberger@gmail.com>
2021-01-07 15:36:28 +0100

    The worddictionary can now be stored and loaded in a json file

--- NaiveBayes/Program.cs
+++ NaiveBayes/Program.cs
@@ -23,14 +23,36 @@ namespace NaiveBayes
         {
             TextFileReader textFileReader = new TextFileReader();
             WordDictionary wordDictionary = new WordDictionary();
+            LoadSaveWordDictionary loadSave = new LoadSaveWordDictionary();
 
             List<string> text = textFileReader.ReadFile(Path.Combine(Directory.GetCurrentDirectory(), "..", "..", "..", "res", "TrainingData", "alt.atheism", "53154"));
-            wordDictionary.AddText(text);
+            AddText(text);
 
-            foreach (string word in wordDictionary.words)
+            void AddText(List<string> text)
+            {
+                foreach (string line in text)
+                {
+                    string[] textWords = line.Split(" ");
+                    foreach (string word in textWords)
+                    {
+                        string newWord = word.Trim(new Char[] { ',', '*', '.', ':', ';', '-', '_', '<', '>', '!', '?' });
+                        if (!newWord.Equals("") && !wordDictionary.Words.Contains(newWord) && !newWord.Contains("@"))
+                        {
+                            wordDictionary.Words.Add(newWord);
+                        }
+                    }
+                }
+            }
+
+            wordDictionary.Words = loadSave.Deserialize();
+
+            foreach (string word in wordDictionary.Words)
             {
                 Console.WriteLine(word);
             }
+
+            //loadSave.Serialize(wordDictionary.Words);
+            
         }
     }
 }
--- NaiveBayes/WordDictionary.cs
+++ NaiveBayes/WordDictionary.cs
@@ -6,22 +6,6 @@ namespace NaiveBayes
 {
     public class WordDictionary
     {
-        public List<string> words = new List<string>();
-
-        public void AddText(List<string> text)
-        {
-            foreach (string line in text)
-            {
-                string[] textWords = line.Split(" ");
-                foreach (string word in textWords)
-                {
-                    string newWord = word.Trim(new Char[] { ',', '*', '.', ':', ';', '-', '_', '<', '>', '!', '?' });
-                    if (!newWord.Equals("") && !words.Contains(newWord) && !newWord.Contains("@"))
-                    {
-                        words.Add(newWord);
-                    }
-                }
-            }
-        }
+        public List<string> Words { get; set; } = new List<string>();
     }
 }

2cb6fcdd8eab1e4663ce03ec23794ecd6b37d3f4
AJannik <jannik.schmidberger@gmail.com>
2021-01-07 14:23:04 +0100

    Add project files.

---
+++ NaiveBayes/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace NaiveBayes
+{
+    class Program
+    {
+        // TODO: Wörterbuch aus den Texten aufbauen
+        // TODO: Absolute Häufigkeiten der Klassen: atheism, graphics, windows, ibm, mac, pc, forsale, autos, motorcycles, baseball, hockey, crypt, electronics, med, space, christian, guns, mideast, politics, religion
+        // TODO: Absolute Häufigkeiten jedes Wortes des Wörterbuchs für jede Klasse speichern
+        // TODO: Unbekannten Text einlesen
+        // TODO: Text anhand des Wörterbuchs in einen Vektor umwandeln
+        // TODO: Vektor mit der Naive-Bayes Formel in eine der 20 Klassen einteilen
+        // TODO: Einteilung überprüfen und gegebenenfalls korrigieren
+
+
+        // TODO: Unbekannte Wörter ins Wörterbuch aufnehmen
+        // TODO: Absolute Häufigkeit der eingeteilten Klasse aktualisieren
+        // TODO: Absolute Häufigkeiten der unbekannten Worter für jede Klasse hinzufügen
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ NaiveBayes/WordDictionary.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NaiveBayes
+{
+    public class WordDictionary
+    {
+        public List<string> words = new List<string>();
+    }
+}
